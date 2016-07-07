f52ae753a6e0cc3eac770941888a66aaa0754404
jessonfoo <jessonfoo@gmail.com>
2016-07-07 12:37:37 -0700

    iadded class for game instantiated game calls as game added initialize method added start method added stop method

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Create a background class for the canvas
+class Game() {
+    this.canvas = null;
+    this.width = 0;
+    this.height = 0;
+}
+var game = new Game();
+
+game.prototype.initialize= function(){
+  /*
+  code code code
+  */
+}
+
+game.prototype.start = function(){
+  /*
+  code code code
+  */
+}
+
+game.prototype.stop = function(){
+  /*
+  code here
+  */
+}
+game.prototype.stop = function(){
+  /*
+  HALT! code here
+  */
+}
